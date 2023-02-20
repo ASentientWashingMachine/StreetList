b8093bc932bb67e5c2d02d1598ff101641a8bc46
Opilane TTHK <opilane@tthk.ee>
2023-02-20 14:12:53 +0200

    The code

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var StreetList = new Dictionary<string, string>();
+
+            StreetList.Add("Kullamäe", "tänav");
+            StreetList.Add("Mihkli", "tänav");
+            StreetList.Add("Kadaka", "tänav");
+            StreetList.Add("Tapa", "tänav");
+            StreetList.Add("Viru", "tänav");
+
+            Console.WriteLine("Tänava List");
+
+            foreach (var street in StreetList)
+            {
+                Console.WriteLine($"{street.Key} {street.Value}");
+            }
+        }
+    }
+}
